70c48821c91d680e10f15070fa196bf06eeb575e
HackerbyOne <trinhminhtri392@gmail.com>
2026-03-30 14:33:11 +0700

    Them co so du lieu

---
+++ myweb.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Chào mừng bạn đến với thế giới PHP!";
+        echo "Hôm nay là ngày: " . date("d/m/Y");
+    ?>
+</body>
+</html>
